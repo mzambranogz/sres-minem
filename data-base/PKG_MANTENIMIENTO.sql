bfa090166c83ab7b29d6d66fab229120cf52f545
RSEANCAS <ronald.jsb@hotmail.com>
2020-06-17 15:10:57 -0500

    Actualización BD

--- data-base/PKG_MANTENIMIENTO.sql
+++ data-base/PKG_MANTENIMIENTO.sql
@@ -1,11 +1,11 @@
 --------------------------------------------------------
--- Archivo creado  - miércoles-junio-17-2020   
+-- Archivo creado  - miï¿½rcoles-junio-17-2020   
 --------------------------------------------------------
 --------------------------------------------------------
 --  DDL for Package PKG_SRES_MANTENIMIENTO
 --------------------------------------------------------
 
-  CREATE OR REPLACE PACKAGE "SRES"."PKG_SRES_MANTENIMIENTO" AS 
+CREATE OR REPLACE NONEDITIONABLE PACKAGE "PKG_SRES_MANTENIMIENTO" AS
 
   PROCEDURE USP_SEL_LISTA_BUSQ_CRITERIO(
     PI_BUSCAR VARCHAR2,
@@ -14,8 +14,15 @@
     PI_COLUMNA VARCHAR2,
     PI_ORDEN VARCHAR2,
     PO_REF OUT SYS_REFCURSOR
-  ); 
+  );
+  
+  PROCEDURE USP_SEL_LISTA_BUSQ_USUARIO(
+    PI_BUSCAR VARCHAR2,
+    PI_REGISTROS NUMBER,
+    PI_PAGINA NUMBER,
+    PI_COLUMNA VARCHAR2,
+    PI_ORDEN VARCHAR2,
+    PO_REF OUT SYS_REFCURSOR
+  );
 
 END PKG_SRES_MANTENIMIENTO;
-
-/
--- data-base/PKG_MANTENIMIENTO_BODY.sql
+++ data-base/PKG_MANTENIMIENTO_BODY.sql
@@ -1,11 +1,11 @@
 --------------------------------------------------------
--- Archivo creado  - mi�rcoles-junio-17-2020   
+-- Archivo creado  - mi�rcoles-junio-17-2020   
 --------------------------------------------------------
 --------------------------------------------------------
 --  DDL for Package Body PKG_SRES_MANTENIMIENTO
 --------------------------------------------------------
 
-  CREATE OR REPLACE PACKAGE BODY "SRES"."PKG_SRES_MANTENIMIENTO" AS
+CREATE OR REPLACE NONEDITIONABLE PACKAGE BODY "PKG_SRES_MANTENIMIENTO" AS
 
   PROCEDURE USP_SEL_LISTA_BUSQ_CRITERIO(
     PI_BUSCAR VARCHAR2,
@@ -14,7 +14,7 @@
     PI_COLUMNA VARCHAR2,
     PI_ORDEN VARCHAR2,
     PO_REF OUT SYS_REFCURSOR
-  ) AS    
+  ) AS
     vTOTAL_REG INTEGER;
     vPAGINA_TOTAL INTEGER;
     vPAGINA_ACTUAL INTEGER := PI_PAGINA;
@@ -25,10 +25,10 @@
   BEGIN
     vQUERY_CONT := 'SELECT  COUNT(1)
                     FROM T_GENM_CRITERIO C
-                    WHERE LOWER(TRANSLATE(C.NOMBRE,''����������'',''AEIOUaeiou'')) like ''%''|| LOWER(TRANSLATE('''|| PI_BUSCAR ||''',''����������'',''AEIOUaeiou'')) ||''%'' AND
+                    WHERE LOWER(TRANSLATE(C.NOMBRE,''??????????'',''AEIOUaeiou'')) like ''%''|| LOWER(TRANSLATE('''|| PI_BUSCAR ||''',''??????????'',''AEIOUaeiou'')) ||''%'' AND
                     FLAG_ESTADO = ''1''';
     EXECUTE IMMEDIATE vQUERY_CONT INTO vTOTAL_REG;
-    
+
     vPAGINA_TOTAL := CEIL(TO_NUMBER(vTOTAL_REG) / TO_NUMBER(PI_REGISTROS));
     IF vPAGINA_ACTUAL = 0 THEN
       vPAGINA_ACTUAL := 1;
@@ -39,8 +39,8 @@
 
     vPAGINA_INICIAL := vPAGINA_ACTUAL - 1;
     vCOLUMNA := PI_COLUMNA;
-    
-    vQUERY_SELECT := 'SELECT * FROM 
+
+    vQUERY_SELECT := 'SELECT * FROM
                         (
                         SELECT  C.ID_CRITERIO,
                                 C.NOMBRE,
@@ -50,14 +50,78 @@
                                 || PI_REGISTROS || ' AS CANTIDAD_REGISTROS,'
                                 || vTOTAL_REG || ' AS TOTAL_REGISTROS
                         FROM T_GENM_CRITERIO C
-                        WHERE LOWER(TRANSLATE(C.NOMBRE,''����������'',''AEIOUaeiou'')) like ''%''|| LOWER(TRANSLATE('''|| PI_BUSCAR ||''',''����������'',''AEIOUaeiou'')) ||''%'' AND
+                        WHERE LOWER(TRANSLATE(C.NOMBRE,''??????????'',''AEIOUaeiou'')) like ''%''|| LOWER(TRANSLATE('''|| PI_BUSCAR ||''',''??????????'',''AEIOUaeiou'')) ||''%'' AND
                         FLAG_ESTADO = ''1''
                         )
                     WHERE  ROWNUMBER BETWEEN ' || TO_CHAR(PI_REGISTROS * vPAGINA_INICIAL + 1) || ' AND ' || TO_CHAR(PI_REGISTROS * (vPAGINA_INICIAL + 1));
-    
+
     OPEN PO_REF FOR vQUERY_SELECT;
   END USP_SEL_LISTA_BUSQ_CRITERIO;
+  
+  PROCEDURE USP_SEL_LISTA_BUSQ_USUARIO(
+    PI_BUSCAR VARCHAR2,
+    PI_REGISTROS NUMBER,
+    PI_PAGINA NUMBER,
+    PI_COLUMNA VARCHAR2,
+    PI_ORDEN VARCHAR2,
+    PO_REF OUT SYS_REFCURSOR
+  ) AS
+    vTOTAL_REG INTEGER;
+    vPAGINA_TOTAL INTEGER;
+    vPAGINA_ACTUAL INTEGER := PI_PAGINA;
+    vPAGINA_INICIAL INTEGER := 0;
+    vQUERY_CONT VARCHAR2(10000) := '';
+    vQUERY_SELECT VARCHAR2(10000) := '';
+    vCOLUMNA VARCHAR2(200);
+  BEGIN
+    vQUERY_CONT := 'SELECT  COUNT(1)
+                    FROM T_GENM_USUARIO U
+                    WHERE (
+                    LOWER(TRANSLATE(U.NOMBRES,''ÁÉÍÓÚáéíóú'',''AEIOUaeiou'')) like ''%''|| LOWER(TRANSLATE('''|| PI_BUSCAR ||''',''ÁÉÍÓÚáéíóú'',''AEIOUaeiou'')) ||''%'' OR
+                    LOWER(TRANSLATE(U.APELLIDOS,''ÁÉÍÓÚáéíóú'',''AEIOUaeiou'')) like ''%''|| LOWER(TRANSLATE('''|| PI_BUSCAR ||''',''ÁÉÍÓÚáéíóú'',''AEIOUaeiou'')) ||''%'' OR
+                    LOWER(TRANSLATE(U.CORREO,''ÁÉÍÓÚáéíóú'',''AEIOUaeiou'')) like ''%''|| LOWER(TRANSLATE('''|| PI_BUSCAR ||''',''ÁÉÍÓÚáéíóú'',''AEIOUaeiou'')) ||''%'' OR
+                    LOWER(TRANSLATE(U.CELULAR,''ÁÉÍÓÚáéíóú'',''AEIOUaeiou'')) like ''%''|| LOWER(TRANSLATE('''|| PI_BUSCAR ||''',''ÁÉÍÓÚáéíóú'',''AEIOUaeiou'')) ||''%''
+                    ) AND
+                    FLAG_ESTADO = ''1''';
+    EXECUTE IMMEDIATE vQUERY_CONT INTO vTOTAL_REG;
+
+    vPAGINA_TOTAL := CEIL(TO_NUMBER(vTOTAL_REG) / TO_NUMBER(PI_REGISTROS));
+    IF vPAGINA_ACTUAL = 0 THEN
+      vPAGINA_ACTUAL := 1;
+    END IF;
+    IF vPAGINA_ACTUAL > vPAGINA_TOTAL THEN
+      vPAGINA_ACTUAL := vPAGINA_TOTAL;
+    END IF;
+
+    vPAGINA_INICIAL := vPAGINA_ACTUAL - 1;
+    vCOLUMNA := PI_COLUMNA;
+
+    vQUERY_SELECT := 'SELECT * FROM
+                        (
+                        SELECT  U.ID_USUARIO,
+                                U.NOMBRES,
+                                U.APELLIDOS,
+                                U.CORREO,
+                                U.TELEFONO,
+                                U.ANEXO,
+                                U.CELULAR,
+                                ROW_NUMBER() OVER (ORDER BY ' || vCOLUMNA || ' ' || PI_ORDEN ||') AS ROWNUMBER,'
+                                || vPAGINA_TOTAL || ' AS TOTAL_PAGINAS,'
+                                || vPAGINA_ACTUAL || ' AS PAGINA,'
+                                || PI_REGISTROS || ' AS CANTIDAD_REGISTROS,'
+                                || vTOTAL_REG || ' AS TOTAL_REGISTROS
+                        FROM T_GENM_USUARIO U
+                        WHERE (
+                        LOWER(TRANSLATE(U.NOMBRES,''ÁÉÍÓÚáéíóú'',''AEIOUaeiou'')) like ''%''|| LOWER(TRANSLATE('''|| PI_BUSCAR ||''',''ÁÉÍÓÚáéíóú'',''AEIOUaeiou'')) ||''%'' OR
+                        LOWER(TRANSLATE(U.APELLIDOS,''ÁÉÍÓÚáéíóú'',''AEIOUaeiou'')) like ''%''|| LOWER(TRANSLATE('''|| PI_BUSCAR ||''',''ÁÉÍÓÚáéíóú'',''AEIOUaeiou'')) ||''%'' OR
+                        LOWER(TRANSLATE(U.CORREO,''ÁÉÍÓÚáéíóú'',''AEIOUaeiou'')) like ''%''|| LOWER(TRANSLATE('''|| PI_BUSCAR ||''',''ÁÉÍÓÚáéíóú'',''AEIOUaeiou'')) ||''%'' OR
+                        LOWER(TRANSLATE(U.CELULAR,''ÁÉÍÓÚáéíóú'',''AEIOUaeiou'')) like ''%''|| LOWER(TRANSLATE('''|| PI_BUSCAR ||''',''ÁÉÍÓÚáéíóú'',''AEIOUaeiou'')) ||''%''
+                        ) AND
+                        FLAG_ESTADO = ''1''
+                        )
+                    WHERE  ROWNUMBER BETWEEN ' || TO_CHAR(PI_REGISTROS * vPAGINA_INICIAL + 1) || ' AND ' || TO_CHAR(PI_REGISTROS * (vPAGINA_INICIAL + 1));
+
+    OPEN PO_REF FOR vQUERY_SELECT;
+  END USP_SEL_LISTA_BUSQ_USUARIO;
 
 END PKG_SRES_MANTENIMIENTO;
-
-/
