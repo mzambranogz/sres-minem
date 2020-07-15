ebd7155a6fceeb24621d59b662ef7269ef1edfbb
RSEANCAS <ronald.jsb@hotmail.com>
2020-07-14 22:03:28 -0500

    Front Inscripción

--- back-end/sres/sres.app/Assets/js/pages/convocatoria_inscribirme.js
+++ back-end/sres/sres.app/Assets/js/pages/convocatoria_inscribirme.js
@@ -13,28 +13,27 @@ var cargarListaInscripcionRequerimiento = () => {
 
 var mostrarListaInscripcionRequerimiento = (data) => {
     if (data.length > 0) {
-        let tituloCriterios = '<div><h3>CRITERIOS GENERALES</h3></div>';
-        let tituloArchivosAdjuntos = '<div><h3>ARCHIVOS ADJUNTOS</h3></div>';
-        let cabecera = `<div>${tituloCriterios}${tituloArchivosAdjuntos}</div>`;
+        let tituloCriterios = '<div class="col-lg-6 col-md-12 col-sm-12"><h3 class="estilo-02 text-sres-azul mb-5">CRITERIOS GENERALES</h3></div>';
+        let tituloArchivosAdjuntos = '<div class="col-lg-6 col-md-12 col-sm-12 d-none d-lg-block"><h3 class="estilo-02 text-sres-azul mb-5">ARCHIVOS ADJUNTOS</h3></div>';
+        let cabecera = `<div class="row">${tituloCriterios}${tituloArchivosAdjuntos}</div>`;
 
         let contenido = data.map(x => {
-            let tituloRequerimiento = `<div><label>${x.REQUERIMIENTO.NOMBRE}</label></div>`;
-            let fileRequerimiento = `<div><input type="file" data-id="${x.ID_REQUERIMIENTO}" name="fileRequerimiento_${x.ID_REQUERIMIENTO}" id="fileRequerimiento_${x.ID_REQUERIMIENTO}" /></div>`;
-            let colLeft = `<div>${tituloRequerimiento}${fileRequerimiento}</div>`;
+            let fileRequerimiento = `<div class="form-group"><label class="estilo-01 text-limit-1" for="fle-requisito-${x.ID_REQUERIMIENTO}">${x.REQUERIMIENTO.NOMBRE}<span class="text-danger font-weight-bold">&nbsp;(*)&nbsp;<i class="fas fa-question-circle ayuda-tooltip" data-toggle="tooltip" data-placement="top" title="Seleccione un archivo para adjuntarlo en el registro de requisitose, se recomienda un archivo del tipo (PDF, DOC, JPG, PNG)"></i></span></label><div class="input-group"><div class="input-group-prepend"><span class="input-group-text"><i class="fas fa-file"></i></span></div><input class="form-control form-control-sm cursor-pointer txt-file-control" type="text" id="txt-requisito-${x.ID_REQUERIMIENTO}" placeholder="Subir documentos" required><input class="d-none fil-file-control" type="file" id="fle-requisito-${x.ID_REQUERIMIENTO}" accept="application/msword, application/vnd.ms-excel, application/vnd.ms-powerpoint, text/plain, application/pdf, image/*"><div class="input-group-append"><label class="input-group-text cursor-pointer estilo-01" for="fle-requisito-${x.ID_REQUERIMIENTO}"><i class="fas fa-upload mr-1"></i>Subir archivo</label></div></div></div>`
+            let colLeft = `<div class="col-lg-6 col-md-12 col-sm-12">${fileRequerimiento}</div>`;
             let existeFileRequerimientoSubido = x.ARCHIVO_BASE || '' != '';
             let contenidoFileRequerimiento = ``
             if (existeFileRequerimientoSubido) {
-                let nombreFileRequerimiento = `<label>ARCHIVO: ${x.ARCHIVO_BASE}</label>`;
-                let btnDescargaFileRequerimiento = `<a href="${baseUrl}api/inscripcionrequerimiento/obtenerarchivo/${idConvocatoria}/${idInscripcion}/${idInstitucionLogin}/${x.ID_REQUERIMIENTO}")">DESCARGAR</a>`;
-                let btnEliminarFileRequerimiento = `<a href="#">ELIMINAR</a>`;
-                contenidoFileRequerimiento = `${nombreFileRequerimiento}${btnDescargaFileRequerimiento}${btnEliminarFileRequerimiento}`;
+                let nombreFileRequerimiento = `<i class="fas fa-check-circle px-2 py-1"></i><span class="estilo-01">${x.ARCHIVO_BASE}</span>`;
+                let btnDescargaFileRequerimiento = `<a class="text-sres-verde" href="${baseUrl}api/inscripcionrequerimiento/obtenerarchivo/${idConvocatoria}/${idInscripcion}/${idInstitucionLogin}/${x.ID_REQUERIMIENTO}"><i class="fas fa-download px-2 py-1"></i></a>`;
+                let btnEliminarFileRequerimiento = `<a class="text-sres-verde" href="#"><i class="fas fa-trash px-2 py-1"></i></a>`;
+                contenidoFileRequerimiento = `<div class="form-group"><label class="estilo-01">&nbsp;</label><div class="alert alert-success p-1 d-flex"><div class="mr-auto">${nombreFileRequerimiento}</div><div class="ml-auto">${btnDescargaFileRequerimiento}${btnEliminarFileRequerimiento}</div></div></div>`;
             }
-            let colRight = `<div>${contenidoFileRequerimiento}</div>`;
-            let contenidoFinal = `<div>${colLeft}${colRight}</div>`;
+            let colRight = `<div class="col-lg-6 col-md-12 col-sm-12">${contenidoFileRequerimiento}</div>`;
+            let contenidoFinal = `<div class="row">${colLeft}${colRight}</div>`;
             return contenidoFinal;
         }).join('')
         $('#lstInscripcionRequerimiento').html(`${cabecera}${contenido}`);
-        $('input[type="file"][id*="fileRequerimiento_"]').on('change', fileRequerimientoChange);
+        $('input[type="file"][id*="txt-requisito-"]').on('change', fileRequerimientoChange);
     }
 }
 
@@ -43,8 +42,13 @@ var btnInscribirmeClick = (e) => {
     enviarInscripcion();
 }
 
+var limpiarFiles = () => {
+    let listaInputFile = $('input[type="file"][id*="txt-requisito-"]');
+    listaInputFile.each(x => $(x).val(''));
+}
+
 var enviarInscripcion = () => {
-    let listaInputFile = $('input[type="file"][id*="fileRequerimiento_"]');
+    let listaInputFile = $('input[type="file"][id*="txt-requisito-"]');
     let listaInscripcionRequerimiento = Array.from(listaInputFile).filter(x => x.files[0] != null)
 
     if (listaInscripcionRequerimiento.length < listaInputFile.length) return;
@@ -99,7 +103,8 @@ var enviarInscripcion = () => {
 var mostrarMensaje = (data) => {
     if (data == true) {
         alert('¡Se guardó correctamente!');
-        mostrarListaInscripcionRequerimiento();
+        limpiarFiles();
+        cargarListaInscripcionRequerimiento();
     }
 }
 
