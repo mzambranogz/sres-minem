5ce299869b21eb17777731a1486bb1f4fa1d518f
JuansotoGrupozuniga <juan.soto@grupo-zuniga.com>
2020-07-22 18:10:24 -0500

    imagenes de criterios en carpetas y se muestran de acuerdo al criterio

--- back-end/sres/sres.be/CriterioBE.cs
+++ back-end/sres/sres.be/CriterioBE.cs
@@ -10,6 +10,9 @@ namespace sres.be
     {
         public int ID_CRITERIO { get; set; }
         public string NOMBRE { get; set; }
+        public string ARCHIVO_BASE { get; set; }
+        public string ARCHIVO_CIFRADO { get; set; }
+        public byte[] ARCHIVO_CONTENIDO { get; set; }
         public string FLAG_ESTADO { get; set; }
     }
 }
