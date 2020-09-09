d143eb6d696431b4f081bd01c3e5c8587b048f0a
JuansotoGrupozuniga <juan.soto@grupo-zuniga.com>
2020-09-09 18:47:11 -0500

    actualizacion cambio de botones menu

---
+++ back-end/sres/sres.app/Assets/js/pages/convocatoria_informacion.js
@@ -0,0 +1,6 @@
+﻿$(document).ready((e) => {
+    $('.barra').removeClass('activo');
+    $('.barra a').removeClass('nav-active');
+    $('.barra-convocatoria').addClass('activo');
+    $('.barra-convocatoria a').addClass('nav-active');
+});
